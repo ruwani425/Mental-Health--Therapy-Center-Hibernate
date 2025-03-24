e66a6e85d0ace577c8d5a2683ac794c17bb117b9
ruwani425 <ruwanielv@gmail.com>
2025-03-24 18:06:55 +0530

    initial commit

--- src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/PatientsViewController.java
+++ src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/PatientsViewController.java
@@ -1,4 +1,94 @@
 package lk.ijse.gdse.SerenityMentalHealthTherapyCenter.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 public class PatientsViewController {
+
+    @FXML
+    private TableView<?> tblPatient;
+
+    @FXML
+    private TableColumn<?, ?> colPatientId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colDateOfBirth;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colPhone;
+
+    @FXML
+    private TableColumn<?, ?> colTherapyProgram;
+
+    @FXML
+    private Label lblPatientId;
+
+    @FXML
+    private JFXTextField txtPatientName;
+
+    @FXML
+    private JFXTextField txtPatientAddress;
+
+    @FXML
+    private JFXTextField txtPatientPhone;
+
+    @FXML
+    private JFXTextField txtPatientEmail;
+
+    @FXML
+    private DatePicker datePickerDob;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnSave;
+
+    @FXML
+    private ComboBox<?> cmbTherapyProgram;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnDeletePatientOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSavePatientOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdatePatientOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(KeyEvent event) {
+
+    }
+
 }
--- src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/TherapistsViewController.java
+++ src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/TherapistsViewController.java
@@ -1,4 +1,20 @@
 package lk.ijse.gdse.SerenityMentalHealthTherapyCenter.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
 public class TherapistsViewController {
+    public JFXButton btnSave;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+
+    public void btnDeleteTherapistOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateTherapistOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveTherapistOnAction(ActionEvent actionEvent) {
+
+    }
 }
