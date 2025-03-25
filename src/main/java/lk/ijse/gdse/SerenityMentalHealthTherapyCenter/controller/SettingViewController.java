48a785cca5e02d93a76f3fea09440c3110dad991
ruwani425 <ruwanielv@gmail.com>
2025-03-25 12:39:02 +0530

    initial commit

--- src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/SettingViewController.java
+++ src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/SettingViewController.java
@@ -1,4 +1,8 @@
 package lk.ijse.gdse.SerenityMentalHealthTherapyCenter.controller;
 
+import com.jfoenix.controls.JFXButton;
+
 public class SettingViewController {
+    public JFXButton btnSubmit;
+    public JFXButton btnAddUser;
 }
