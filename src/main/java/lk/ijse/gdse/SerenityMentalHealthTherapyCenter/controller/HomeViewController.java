6c8e68af977081a4e11c40ab2f109dec3474b642
ruwani425 <ruwanielv@gmail.com>
2025-04-30 10:21:41 +0530

    initial commit

--- src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/HomeViewController.java
+++ src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/HomeViewController.java
@@ -12,6 +12,10 @@ import java.util.ResourceBundle;
 public class HomeViewController implements Initializable {
 
     @FXML
+    private Label lblDate;
+    @FXML
+    private Label lblTime;
+    @FXML
     private Label lblProgramsTotal;
     @FXML
     private Label lblPatientsTotal;
@@ -22,11 +26,25 @@ public class HomeViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        lblDate.setText(java.time.LocalDate.now().toString());
+
+        javafx.animation.Timeline timeline = new javafx.animation.Timeline(
+                new javafx.animation.KeyFrame(javafx.util.Duration.seconds(1), event -> {
+                    java.time.LocalTime currentTime = java.time.LocalTime.now();
+                    lblTime.setText(currentTime.format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")));
+                })
+        );
+        timeline.setCycleCount(javafx.animation.Animation.INDEFINITE);
+        timeline.play();
+
         long patientCount = homeBO.getPatientCount();
         lblPatientsTotal.setText(String.valueOf(patientCount));
+
         long totalPrograms = homeBO.getTotalProgramsCount();
         lblProgramsTotal.setText(String.valueOf(totalPrograms));
+
         long totalTherapists = homeBO.getTotalTherapistsCount();
         lblTherapistTotal.setText(String.valueOf(totalTherapists));
     }
+
 }
--- src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/LoginViewController.java
+++ src/main/java/lk/ijse/gdse/SerenityMentalHealthTherapyCenter/controller/LoginViewController.java
@@ -6,10 +6,14 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import lk.ijse.gdse.SerenityMentalHealthTherapyCenter.bo.BOFactory;
 import lk.ijse.gdse.SerenityMentalHealthTherapyCenter.bo.custom.UserBO;
@@ -19,9 +23,18 @@ import lk.ijse.gdse.SerenityMentalHealthTherapyCenter.dto.UserDTO;
 import lk.ijse.gdse.SerenityMentalHealthTherapyCenter.entity.Appointment;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class LoginViewController {
+public class LoginViewController implements Initializable {
+
+    @FXML
+    private ImageView imgClosedEye;
+    @FXML
+    private JFXTextField txtShowPassword;
+    @FXML
+    private ImageView imgOpenEye;
     @FXML
     private Label lblFogotPassword;
     @FXML
@@ -32,6 +45,13 @@ public class LoginViewController {
     private JFXButton loginBtn;
 
     private final UserBO userBO = (UserBO) BOFactory.getInstance().getBO(BOFactory.BOType.USER);
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        txtShowPassword.setVisible(false);
+        imgOpenEye.setVisible(false);
+    }
 
     public void NavigateToDashboard(ActionEvent actionEvent) {
         String username = txtUserName.getText();
@@ -77,5 +97,33 @@ public class LoginViewController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void hidePasswordOnAction(KeyEvent keyEvent) {
+        String validPassword = txtPassword.getText();
+        txtShowPassword.setText(validPassword);
+    }
+
+    @FXML
+    private void showPasswordOnAction(KeyEvent keyEvent) {
+        String validPassword = txtShowPassword.getText();
+        txtPassword.setText(validPassword);
+    }
+
+    @FXML
+    private void closeEyeClickOnAction(MouseEvent mouseEvent) {
+        txtShowPassword.setVisible(true);
+        imgOpenEye.setVisible(true);
+        imgClosedEye.setVisible(false);
+        txtPassword.setVisible(false);
+    }
+
+    @FXML
+    private void openEyeClickOnAction(MouseEvent mouseEvent) {
+        txtShowPassword.setVisible(false);
+        imgOpenEye.setVisible(false);
+        imgClosedEye.setVisible(true);
+        txtPassword.setVisible(true);
     }
 }
